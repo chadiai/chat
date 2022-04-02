1dc8d42a0a4d2819a8f1d5a3b1136a938b8d96dd
chadiai <r0786882@student.thomasmore.be>
2022-04-02 20:59:13 +0200

    chat database

--- components/Chat.js
+++ components/Chat.js
@@ -2,41 +2,72 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import { getAuth, signOut } from "firebase/auth";
 import { useAuth } from "../auth";
+import { db } from "../firebase/firebaseDatabase";
+import { collection, addDoc } from "@firebase/firestore"
+import { useCollection } from "react-firebase-hooks/firestore"
 
 
 const SideBar = () => {
     const [userLookup, setUserLookup] = useState("");
     
     const { user } = useAuth();
-    
-    const userlookup = async (e) => {
-        e.preventDefault();
-        console.log("pressed")
+    const [ snapshotChats ] = useCollection(collection(db, "chats"));
+    const chats = snapshotChats?.docs.map((doc => ({id: doc.id, ...doc.data()})));
 
+    const [ snapshotUsers ] = useCollection(collection(db, "users"));
+    const users = snapshotUsers?.docs.map((doc => ({id: doc.id, ...doc.data()})));
+
+    const chatExists = email => chats?.fill(chat => {chat.emails.includes(user.email) && chat.emails.includes(email)})
+
+    const userlookup = async () => {
+        const input = prompt("Enter email");
+
+        if (!chatExists(input) && (input != user.email)){
+            await addDoc(collection(db,"chats"),{emails: [user.email, input],users: [user.displayName, input]})
+        }
+        
     }
 
+    
+    const UserPreview = props => {
+        return (
+            <div id="friend-username">
+                <i className="fa-solid fa-user"></i>
+                {props.name}
+            </div>
+        )
+    }
+
+    const getUser = (users, currentUser) => {
+        return users?.filter(user => user!== currentUser.displayName)[0];
+    
+    }
+
+    const UserList = () => {
+        return (
+        <>
+            {chats?.filter(chat => chat.id.includes(user.uid))
+            .map(
+                chat => 
+                <UserPreview key={chat.users} name={getUser(chat.users,user)} /> )}
+        </>
+        )
+    }
+    
+    
     return (
         <div className="preview" >
-            <UserPreview name={user.displayName}/>
-            
+            <UserPreview name={user.displayName}/>            
             <form onSubmit={userlookup}>
                 <input id="userlookup" onChange={(e) => setUserLookup(e.target.value)}
                         value={userLookup}></input>
                 <button id="newchat" type="submit" > new chat</button>
             </form>
-            <UserPreview name="user"/>
+            <UserList/>
         </div>    
     )
 }
 
-const UserPreview = props => {
-    return (
-        <div id="friend-username">
-            <i class="fa-solid fa-user"></i>
-            {props.name}
-        </div>
-    )
-}
 
 const Chatbox = () => {
     return (
